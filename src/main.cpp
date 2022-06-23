04224b746e175af2661f4492a6f6be5433f422e0
mva-one <marcell@mva-one.de>
2022-06-23 19:14:57 +0200

    WiFiManager done!

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(115200);
+  WiFiManager wifiManager;
+
+  // reset saved settings
+  // wifiManager.resetSettings();
+
+  wifiManager.autoConnect("GCtl-Client-AutoConnect");
+
+  // if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
